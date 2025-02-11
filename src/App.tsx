2bb8db90843da649e17039e223af953123328e13
Emad Cheshmazar <cheshmazaremad@gmail.com>
2025-02-11 11:06:44 +0330

    fix: create routes enums

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ReduxProvider from "./redux/provider";
-import Home from "./pages/Home";
 import TaskManager from "./pages/TaskManager";
+import { ERoutes } from "./constants/routes";
 
 const App: React.FC = () => {
   return (
     <ReduxProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/my-tasks" element={<TaskManager />} />
+          <Route path={ERoutes.home} element={<TaskManager />} />
         </Routes>
       </Router>
     </ReduxProvider>
